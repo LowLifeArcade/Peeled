1dfc71962cd072a500fe492733d8a44f65b720b3
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2020-10-13 10:40:27 -0700

    added redux-form to submit streams or 'peels'

--- src/components/Peels/PeelsCreate.js
+++ src/components/Peels/PeelsCreate.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const PeelsCreate = () => {
-  return <div>PeelsCreate</div>
+class PeelsCreate extends React.Component {
+  renderError({ error, touched }) {
+    if(touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className=`field ${meta.error && meta.touched ? 'error': ''}`
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    )
+  }
+
+  // use this to send to API for autonaming idea
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error" >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <br />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
 };
 
-export default PeelsCreate;
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'Please enter a title'
+  }
+
+  if (!formValues.description) {
+    errors.description = 'Please enter description'
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'peelsCreate',
+  validate
+})(PeelsCreate);
