9249bde4899dac9374aa6b4d818b54bfddd88a74
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2020-10-12 18:45:36 -0700

    added auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,57 @@
 import React from 'react'
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '841043464753-5fid4h1f1gsfj343jrvq2i1uii507cmj.apps.googleusercontent.com',
         scope: 'email'
-      })
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
+        
+      });
     });
   }
-  render() {
-    return <div>Google Auth</div>
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      )
+    } else {
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign in with Goolge
+        </button>
+      )
+    }
   }
-};
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  render() {
+    return <div>{this.renderAuthButton()} </div>;
+  }
+}
 
 export default GoogleAuth;
